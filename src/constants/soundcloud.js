231c313ad735b0d8774ff3ed32d158ae731ae51c
Nicholas Harvey <nicholasharvey@new-host-5.home>
2021-03-01 14:37:55 -0800

    change api query field from tags to genres, change quest string to 'zelda'

--- src/constants/soundcloud.js
+++ src/constants/soundcloud.js
@@ -1,2 +1,2 @@
 export const SONGS_ENDPOINT =
-  'https://api.soundcloud.com/tracks?linked_partitioning=1&limit=20&offset=0&tags=LOFI&client_id=';
+  'https://api.soundcloud.com/tracks?linked_partitioning=1&limit=20&offset=0&genres=zelda&client_id=';
