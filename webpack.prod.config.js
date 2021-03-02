9c915425a922925195fe17894268352dbe99bb58
Nicholas Harvey <nicholasharvey@new-host-5.home>
2021-03-02 12:40:43 -0800

    update webpack, webpack-cli, webpack-dev-server to latest versions

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,38 +1,22 @@
 var CompressionPlugin = require('compression-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const path = require('path');
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
-
-const extractSass = new ExtractTextPlugin({
-  filename: 'styles.css',
-  disable: process.env.NODE_ENV === 'development',
-});
 
 module.exports = {
   devtool: 'source-map',
   mode: 'production',
-
-  entry: ['babel-polyfill', './src/index.jsx'],
+  entry: ['regenerator-runtime/runtime', './src/index.jsx'],
   output: {
-    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',
   },
   plugins: [
-    new ExtractTextPlugin('styles.scss'),
-    extractSass,
 
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
       },
     }),
-    new SWPrecacheWebpackPlugin({
-      cacheId: 'tester',
-      filename: 'my-service-worker.js',
-    }),
+
     new CompressionPlugin({
       filename: '[path].gz[query]',
       algorithm: 'gzip',
@@ -48,26 +32,7 @@ module.exports = {
 
   module: {
     rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        use: extractSass.extract({
-          use: [
-            {
-              loader: 'css-loader',
-            },
-            {
-              loader: 'sass-loader',
-            },
-          ],
-          // use style-loader in development
-          fallback: 'style-loader',
-        }),
-      },
+      {test: /\.jsx?$/, exclude: /node_modules/, use: 'babel-loader'},
     ],
   },
   resolve: {
