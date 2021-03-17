9082ada8afd84f215cb361daaefbafd6a48bd415
Nicholas Harvey <nharvey278@gmail.com>
2021-03-17 14:02:23 -0700

    remove explicit React.Fragment html tag in favor of empty tag convention

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const Title = ({track, track:{title, user}}) => (
-  <React.Fragment>
+  <>
     <figcaption className="text-center text-sm pt-3">
       {user.username}
     </figcaption>
     <Link to={`track/${track.id}`}>
       <figcaption className="text-white text-center text-xs ">{title}</figcaption>
     </Link>
-  </React.Fragment>
+  </>
 );
 
 Title.propTypes = {
