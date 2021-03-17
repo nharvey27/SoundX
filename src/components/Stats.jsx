abcd99176f43d6bf261f3fc812e25103ef0d3e6a
Nicholas Harvey <nharvey278@gmail.com>
2021-03-17 14:01:21 -0700

    convert to functional componenet'

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,33 +1,26 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-class Stats extends React.Component {
-  isZero = stat => stat || 0;
+const Stats = ({track: {playback_count, favoritings_count, comment_count}}) => {
+  let isZero = stat => stat || 0;
 
-  render() {
-    const {
-      playback_count,
-      favoritings_count,
-      comment_count,
-    } = this.props.track;
-    return (
-      <div className="flex justify-center">
-        <div className="stat">
-          <i className="material-icons">play_arrow</i>
-          <span>{this.isZero(playback_count)}</span>
-        </div>
-        <div className="stat">
-          <i className="material-icons">favorite</i>
-          <span>{this.isZero(favoritings_count)}</span>
-        </div>
-        <div className="stat">
-          <i className="material-icons">chat_bubble</i>
-          <span>{this.isZero(comment_count)}</span>
-        </div>
+  return (
+    <div className="flex justify-center">
+      <div className="stat">
+        <i className="material-icons">play_arrow</i>
+        <span>{isZero(playback_count)}</span>
       </div>
-    );
-  }
-}
+      <div className="stat">
+        <i className="material-icons">favorite</i>
+        <span>{isZero(favoritings_count)}</span>
+      </div>
+      <div className="stat">
+        <i className="material-icons">chat_bubble</i>
+        <span>{isZero(comment_count)}</span>
+      </div>
+    </div>
+  );
+};
 
 Stats.propTypes = {
   playback_count: PropTypes.number.isRequired,
